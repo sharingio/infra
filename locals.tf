ff16cb217ce2d700c1a36730f6ea8a4e63ef647d
hh <hh@ii.coop>
2024-04-10 19:36:39 +0000

    chore: update talos to v1.7.0-beta.0
    We need the bond0 interface to be constructed correctly.
    - https://github.com/siderolabs/talos/issues/8443
    
    We also need BPG routes to be pushed correctly.
    - https://github.com/siderolabs/talos/issues/8443
    
    > In fact, this issue should be resolved by now (in 1.7-beta.0 as it
    gets cut), but if you see it once again, please post with the metadata
    or a way to reproduce.

--- locals.tf
+++ locals.tf
@@ -1,13 +1,13 @@
 locals {
   # NOTE ipxe_script_url must be not contain extensions and must contain the following kernel args
   #   console=ttyS1,115200n8  talos.platform=equinixMetal
-  ipxe_script_url = "https://pxe.factory.talos.dev/pxe/32b31bd7a77a4c38529e38125c50282ae481723f25aa911c4c8a658638fe16d0/v1.6.6/metal-amd64"
+  ipxe_script_url = "https://pxe.factory.talos.dev/pxe/b5b2a4d2bd7add4eff1649fd7e580b61f376313aba7e5e3227bae12f45a0748b/v1.7.0-beta.0/metal-amd64"
   # NOTE install image must contain the following kernel args
   #   console=ttyS1,115200n8  talos.platform=equinixMetal
-  talos_install_image     = "factory.talos.dev/installer/04e08b65e14d351ab85789fd0b0d73705a29397288ec0b77a13e9dd0ea18d08b:v1.6.6"
-  talos_version           = "v1.6.6"
+  #   It also needs gvisor, iscsi-tools, and mdadm extensions
+  talos_install_image     = " factory.talos.dev/installer/25889b382ca7647e59d2a22d4cbb535e30d224f751b7e1d6ac677fb96fa1002d:v1.7.0-beta.0"
+  talos_version           = "v1.7.0-beta"
   kubernetes_version      = "v1.29.2"
-  acme_email_address      = "acme@ii.coop"
   rfc2136_algorithm       = "HMACSHA256"
   k8s_apiserver_subdomain = "k8s"
 }
