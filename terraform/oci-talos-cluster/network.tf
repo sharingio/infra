dd57f15ec6189bc2d061b9616aa36f5319ef0c98
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-08-19 12:19:50 +1200

    chore: fix health check on apiserver
    expect 200 because of anonymous auth

--- terraform/oci-talos-cluster/network.tf
+++ terraform/oci-talos-cluster/network.tf
@@ -156,7 +156,7 @@ resource "oci_network_load_balancer_backend_set" "controlplane_backend_set" {
     #Optional
     interval_in_millis = 10000
     port               = 6443
-    return_code        = 401
+    return_code        = 200
     url_path           = "/readyz"
   }
   #Optional
