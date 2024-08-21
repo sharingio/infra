eb7a8dbac49e15ae40a830eacdea29ee611ba724
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-08-22 11:44:15 +1200

    chore: update identity group
    use simpler config which should target resources

--- terraform/oci-talos-cluster/iam.tf
+++ terraform/oci-talos-cluster/iam.tf
@@ -1,20 +1,11 @@
-resource "oci_identity_group" "this" {
-  name           = var.cluster_name
-  description    = "manage resources"
-  compartment_id = var.tenancy_ocid
-}
-
 resource "oci_identity_dynamic_group" "instance_dynamic_group" {
   #Required
-  compartment_id = var.compartment_ocid
+  name           = "${var.cluster_name}-oci-ccm"
+  compartment_id = var.tenancy_ocid
   description    = "Instance access"
   matching_rule  = <<EOF
-allow dynamic-group ${oci_identity_group.this.name} to read instance-family in compartment ${data.oci_identity_compartment.this.name}
-allow dynamic-group ${oci_identity_group.this.name} to use virtual-network-family in compartment ${data.oci_identity_compartment.this.name}
-allow dynamic-group ${oci_identity_group.this.name} to manage load-balancers in compartment ${data.oci_identity_compartment.this.name}
-allow dynamic-group ${oci_identity_group.this.name} to manage volume-family in compartment ${data.oci_identity_compartment.this.name}
+ALL {instance.compartment.id = '${var.compartment_ocid}'}
 EOF
-  name           = oci_identity_group.this.name
 
   #Optional
   freeform_tags = local.common_labels
