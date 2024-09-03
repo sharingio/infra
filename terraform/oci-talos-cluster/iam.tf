598885e9284ff65b79b7f1d285705da2f52e05bf
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-09-04 10:16:50 +1200

    chore: update oci identity policy for ccm
    attempt identity policy for ccm

--- terraform/oci-talos-cluster/iam.tf
+++ terraform/oci-talos-cluster/iam.tf
@@ -1,4 +1,4 @@
-resource "oci_identity_dynamic_group" "instance_dynamic_group" {
+resource "oci_identity_dynamic_group" "oci-ccm" {
   #Required
   name           = "${var.cluster_name}-oci-ccm"
   compartment_id = var.tenancy_ocid
@@ -6,6 +6,27 @@ resource "oci_identity_dynamic_group" "instance_dynamic_group" {
   matching_rule  = <<EOF
 ALL {instance.compartment.id = '${var.compartment_ocid}'}
 EOF
+
+  #Optional
+  freeform_tags = local.common_labels
+}
+
+resource "oci_identity_policy" "oci-ccm" {
+  #Required
+  name           = "${var.cluster_name}-oci-ccm"
+  compartment_id = var.tenancy_ocid
+  description    = "Instance access"
+  statements = [
+    // LoadBalancer Services
+    "allow dynamic-group ${var.cluster_name}-oci-ccm to read instance-family in compartment ${data.oci_identity_compartment.this.name}",
+    "allow dynamic-group ${var.cluster_name}-oci-ccm to use virtual-network-family in compartment ${data.oci_identity_compartment.this.name}",
+    "allow dynamic-group ${var.cluster_name}-oci-ccm to manage load-balancers in compartment ${data.oci_identity_compartment.this.name}",
+
+    // CSI
+    "allow dynamic-group ${var.cluster_name}-oci-ccm to read instance-family in compartment ${data.oci_identity_compartment.this.name}",
+    "allow dynamic-group ${var.cluster_name}-oci-ccm to use virtual-network-family in compartment ${data.oci_identity_compartment.this.name}",
+    "allow dynamic-group ${var.cluster_name}-oci-ccm to manage volume-family in compartment ${data.oci_identity_compartment.this.name}",
+  ]
 
   #Optional
   freeform_tags = local.common_labels
