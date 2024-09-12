843d1ab7308ddff988be34b15402465eaa6b7dde
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-09-13 09:16:00 +1200

    chore: try updated policy
    use specified compartment and dynamic-group name

--- terraform/oci-talos-cluster/iam.tf
+++ terraform/oci-talos-cluster/iam.tf
@@ -5,11 +5,11 @@ resource "oci_identity_policy" "oci_ccm" {
   description    = "Instance access"
   statements = [
     // LoadBalancer Services
-    "Allow dynamic-group test to read instance-family in compartment ${data.oci_identity_compartment.this.name}",
-    "Allow dynamic-group test to use virtual-network-family in compartment ${data.oci_identity_compartment.this.name}",
-    "Allow dynamic-group test to manage load-balancers in compartment ${data.oci_identity_compartment.this.name}",
+    "Allow dynamic-group 'cloudnative.coop'/'test' to read instance-family in compartment ${data.oci_identity_compartment.this.name}",
+    "Allow dynamic-group 'cloudnative.coop'/'test' to use virtual-network-family in compartment ${data.oci_identity_compartment.this.name}",
+    "Allow dynamic-group 'cloudnative.coop'/'test' to manage load-balancers in compartment ${data.oci_identity_compartment.this.name}",
     // CSI
-    "Allow dynamic-group test to manage volume-family in compartment ${data.oci_identity_compartment.this.name}",
+    "Allow dynamic-group 'cloudnative.coop'/'test' to manage volume-family in compartment ${data.oci_identity_compartment.this.name}",
   ]
 
   #Optional
