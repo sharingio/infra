8de488dae8c5613f5cb0431ebe0e3d29d4ea0b05
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-09-26 11:57:13 +1200

    chore: add outputs in main
    output from module

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,22 @@
+output "factory_disk_image" {
+  value = module.cluster-sharingio-oci.factory_disk_image
+}
 
+output "load_balancer_ip" {
+  value = module.cluster-sharingio-oci.load_balancer_ip
+}
+
+output "talosconfig" {
+  value     = module.cluster-sharingio-oci.talosconfig
+  sensitive = true
+}
+
+output "kubeconfig" {
+  value     = module.cluster-sharingio-oci.kubeconfig
+  sensitive = true
+}
+
+output "oci_cloud_provider_config" {
+  value     = module.cluster-sharingio-oci.oci_cloud_provider_config
+  sensitive = true
+}
